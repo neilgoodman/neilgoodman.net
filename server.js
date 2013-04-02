b5d375c0dd2974cad84cd4fc233c43610e5f521d
posco2k8 <posco2k8@github.fake>
2013-04-02 00:02:53 -0500

    Running wheat through connect. Hooking up connect-static and connect-assets. Importing jQuery, Underscore, and Twitter Bootstrap.

--- server.js
+++ server.js
@@ -3,8 +3,28 @@
  */
 
 var wheat = require('wheat'),
-    http = require('http');
+    connect = require('connect'),
+    http = require('http'),
+    connectAssets = require('connect-assets');
 
-http
-    .createServer(wheat(process.env.PWD))
-    .listen(8888);
+var app = connect();
+app
+    .use(connect.compress())
+    .use(connect.static(__dirname + '/skin/public'))
+    .use(connectAssets({
+        src: __dirname + '/skin/public'
+    }))
+    .use(connect.errorHandler())
+    .use(function (req, res, next) {
+        if (global.js) {
+            global.js.root = '/javascripts';
+        }
+
+        if (global.css) {
+            global.css.root = '/stylesheets';
+        }
+        
+        var wheatProcess = wheat(__dirname);
+        wheatProcess.call(this, req, res, next);
+    })
+    .listen(3000);
---
+++ skin/public/javascripts/javascripts.js
@@ -0,0 +1,15 @@
+//= require jquery-1.9.1
+//= require underscore
+//= require bootstrap/bootstrap-affix
+//= require bootstrap/bootstrap-alert
+//= require bootstrap/bootstrap-button
+//= require bootstrap/bootstrap-carousel
+//= require bootstrap/bootstrap-collapse
+//= require bootstrap/bootstrap-dropdown
+//= require bootstrap/bootstrap-modal
+//= require bootstrap/bootstrap-tooltip
+//= require bootstrap/bootstrap-popover
+//= require bootstrap/bootstrap-scrollspy
+//= require bootstrap/bootstrap-tab
+//= require bootstrap/bootstrap-transition
+//= require bootstrap/bootstrap-typeahead
