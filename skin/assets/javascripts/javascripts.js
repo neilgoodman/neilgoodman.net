8596acea089a4a9501a117009a7c0b4d8fd5fefd
posco2k8 <posco2k8@github.fake>
2013-07-04 18:16:02 -0500

    Twitter sidebar now has loading throbber. Contact sidebar finished. Article page theme started.

--- skin/assets/javascripts/javascripts.js
+++ skin/assets/javascripts/javascripts.js
@@ -2,6 +2,7 @@
 //= require underscore
 //= require prettify
 //= require moment
+//= require spin
 //= require bootstrap/bootstrap-affix
 //= require bootstrap/bootstrap-alert
 //= require bootstrap/bootstrap-button
@@ -27,10 +28,20 @@ $(function () {
 $(function () {
     "use strict";
 
+    setupSpinner();
+
     // Templates
     var tweetListTemplate = _.template($('[data-template-name="tweet-list"]').html()),
         success = function (tweets) {
-            $('.tweet-list').append($.parseHTML(tweetListTemplate({ tweets: tweets })));
+            var $tweetList = $($.parseHTML(tweetListTemplate({ tweets: tweets })));
+
+            $('.tweet-list')
+                .empty()
+                .append($tweetList);
+
+            _.defer(function () {
+                $tweetList.addClass('in');
+            });
         };
 
     // Request tweets through local proxy.
@@ -48,4 +59,28 @@ function parseTweet(tweet) {
     tweet = tweet.replace(userNamesPattern, '<a target="_blank" href="//twitter.com/$1">$&</a>');
 
     return tweet;
+}
+
+function setupSpinner() {
+    var opts = {
+        lines: 13, // The number of lines to draw
+        length: 6, // The length of each line
+        width: 2, // The line thickness
+        radius: 5, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 0, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        color: '#000', // #rgb or #rrggbb
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: 'auto', // Top position relative to parent in px
+        left: 'auto' // Left position relative to parent in px
+    };
+
+    var target = $('.tweet-list')[0];
+    var spinner = new Spinner(opts).spin(target);
 }
