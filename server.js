42a92ca4a0663f786467ba7b461d63cb5f550d23
posco2k8 <posco2k8@github.fake>
2013-08-10 22:55:38 -0500

    Adding remaining aliases. Updating feed template.

--- server.js
+++ server.js
@@ -11,7 +11,19 @@ var less = require('less'),
 
 var app = connect(),
     aliases = {
-        'cordova-html5-tutorial-1': '2012/07/15/building-an-ios-and-android-html5-app-with-cordova-part-1'
+        'cordova-html5-tutorial-1': '2012/07/15/building-an-ios-and-android-html5-app-with-cordova-part-1',
+        'thoughts-on-html5-based-mobile-apps': '2012/07/01/thoughts-on-html5-based-mobile-apps',
+        'five-important-things-in-web-development': '2012/05/05/five-important-things-to-know-in-web-development',
+        'responsive-design-for-the-blog': '2012/03/18/new-responsive-design-for-the-blog',
+        'android-fragments-2': '2012/03/12/working-with-fragments-on-android-part-2',
+        'life-updates': '2012/03/11/life-updates-first-of-many',
+        'android-fragments-1': '2012/01/29/working-with-fragments-on-android-part-1',
+        'social-button-alignment': '2012/01/14/making-social-buttons-line-up-in-a-row-in-css',
+        'stack-overflow-comments-in-wordpress': '2012/01/07/building-stack-overflow-style-code-blocks-on-wordpress',
+        'android-rest-client-2': '2012/01/01/modern-techniques-for-implementing-rest-clients-on-android-4-0-and-below-part-2/',
+        'android-2-1-setimageuri': '2011/12/28/using-imageviews-setimageuri-method-on-android-2-1',
+        'android-rest-client-1': '2011/12/26/modern-techniques-for-implementing-rest-clients-on-android-4-0-and-below-part-1',
+        'welcome': '2011/12/24/welcome-to-the-blog'
     },
     wheatProcess = wheat(__dirname, aliases);
 
